4fb86dd32bd7365637daf5b4f689347539ab742f
ckabuo <ckabuo@purdue.edu>
2024-11-19 23:32:53 -0500

    This one na modern day WTR

--- component/head.tsx
+++ component/head.tsx
@@ -7,26 +7,26 @@ const Head = () => {
       <meta name="viewport" content="width=device-width" />
       <meta
         name="description"
-        content="E Good Make I Deploy Today | Reminder for all dem Developers, Designers and Ogas at the top"
+        content="E Good Make I Deploy Today? | Reminder for all dem Developers, Designers and Ogas at the top"
       />
 
       <meta itemProp="name" content="Make I Deploy Today?" />
       <meta
         itemProp="description"
-        content="E Good Make I Deploy Today | Reminder for all dem Developers, Designers and Ogas at the top"
+        content="E Good Make I Deploy Today? | Reminder for all dem Developers, Designers and Ogas at the top"
       />
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:title" content="Make I Deploy Today?" />
       <meta
         name="twitter:description"
-        content="E Good Make I Deploy Today | Reminder for all dem Developers, Designers and Ogas at the top"
+        content="E Good Make I Deploy Today? | Reminder for all dem Developers, Designers and Ogas at the top"
       />
 
       <meta property="og:title" content="Make I Deploy Today?" />
       <meta
         property="og:description"
-        content="E Good Make I Deploy Today | Reminder for all dem Developers, Designers and Ogas at the top"
+        content="E Good Make I Deploy Today? | Reminder for all dem Developers, Designers and Ogas at the top"
       />
       <meta property="og:url" content="https://makeideploy.today" />
       <meta property="og:site_name" content="E Good Make I Deploy Today?" />
