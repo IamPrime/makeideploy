1257069b8ddc9821ee2dfed5382674f62fbb32d7
I_am_Prime <chimdiaprimus.kabuo@yandex.com>
2026-03-29 18:28:27 -0400

    fix(extension): localize UI and mirror website design
    - Remove English from language options; default to Pidgin (pcm)
    - Use API-returned tagline so it updates with selected locale
    - Add BADGE_TRANSLATIONS and UI_TRANSLATIONS for per-locale yes/no
      badge, timezone/language labels, and button text
    - Redesign popup to match website: reason text is the hero element,
      background switches to #b4221b on no-deploy (mirrors .its-friday),
      tagline is small and secondary
    - Fix muted text colors on red background to use solid rgba(26,26,26)
      with text-shadow, matching the website's .its-friday approach

--- extension/popup.js
+++ extension/popup.js
@@ -1,13 +1,114 @@
+// Timezone data (from helpers/timezones.ts)
+const TIMEZONES = [
+  'Africa/Abidjan', 'Africa/Accra', 'Africa/Addis_Ababa', 'Africa/Algiers', 'Africa/Asmara', 'Africa/Asmera', 'Africa/Bamako', 'Africa/Bangui', 'Africa/Banjul', 'Africa/Bissau', 'Africa/Blantyre', 'Africa/Brazzaville', 'Africa/Bujumbura', 'Africa/Cairo', 'Africa/Casablanca', 'Africa/Ceuta', 'Africa/Conakry', 'Africa/Dakar', 'Africa/Dar_es_Salaam', 'Africa/Djibouti', 'Africa/Douala', 'Africa/El_Aaiun', 'Africa/Freetown', 'Africa/Gaborone', 'Africa/Harare', 'Africa/Johannesburg', 'Africa/Juba', 'Africa/Kampala', 'Africa/Khartoum', 'Africa/Kigali', 'Africa/Kinshasa', 'Africa/Lagos', 'Africa/Libreville', 'Africa/Lome', 'Africa/Luanda', 'Africa/Lubumbashi', 'Africa/Lusaka', 'Africa/Malabo', 'Africa/Maputo', 'Africa/Maseru', 'Africa/Mbabane', 'Africa/Mogadishu', 'Africa/Monrovia', 'Africa/Nairobi', 'Africa/Ndjamena', 'Africa/Niamey', 'Africa/Nouakchott', 'Africa/Ouagadougou', 'Africa/Porto-Novo', 'Africa/Sao_Tome', 'Africa/Timbuktu', 'Africa/Tripoli', 'Africa/Tunis', 'Africa/Windhoek', 'America/Adak', 'America/Anchorage', 'America/Anguilla', 'America/Antigua', 'America/Araguaina', 'America/Argentina/Buenos_Aires', 'America/Argentina/Catamarca', 'America/Argentina/ComodRivadavia', 'America/Argentina/Cordoba', 'America/Argentina/Jujuy', 'America/Argentina/La_Rioja', 'America/Argentina/Mendoza', 'America/Argentina/Rio_Gallegos', 'America/Argentina/Salta', 'America/Argentina/San_Juan', 'America/Argentina/San_Luis', 'America/Argentina/Tucuman', 'America/Argentina/Ushuaia', 'America/Aruba', 'America/Asuncion', 'America/Atikokan', 'America/Atka', 'America/Bahia', 'America/Bahia_Banderas', 'America/Barbados', 'America/Belem', 'America/Belize', 'America/Blanc-Sablon', 'America/Boa_Vista', 'America/Bogota', 'America/Boise', 'America/Buenos_Aires', 'America/Cambridge_Bay', 'America/Campo_Grande', 'America/Cancun', 'America/Caracas', 'America/Catamarca', 'America/Cayenne', 'America/Cayman', 'America/Chicago', 'America/Chihuahua', 'America/Coral_Harbour', 'America/Cordoba', 'America/Costa_Rica', 'America/Creston', 'America/Cuiaba', 'America/Curacao', 'America/Danmarkshavn', 'America/Dawson', 'America/Dawson_Creek', 'America/Denver', 'America/Detroit', 'America/Dominica', 'America/Edmonton', 'America/Eirunepe', 'America/El_Salvador', 'America/Ensenada', 'America/Fort_Nelson', 'America/Fort_Wayne', 'America/Fortaleza', 'America/Glace_Bay', 'America/Godthab', 'America/Goose_Bay', 'America/Grand_Turk', 'America/Grenada', 'America/Guadeloupe', 'America/Guatemala', 'America/Guayaquil', 'America/Guyana', 'America/Halifax', 'America/Havana', 'America/Hermosillo', 'America/Indiana/Indianapolis', 'America/Indiana/Knox', 'America/Indiana/Marengo', 'America/Indiana/Petersburg', 'America/Indiana/Tell_City', 'America/Indiana/Vevay', 'America/Indiana/Vincennes', 'America/Indiana/Winamac', 'America/Indianapolis', 'America/Inuvik', 'America/Iqaluit', 'America/Jamaica', 'America/Jujuy', 'America/Juneau', 'America/Kentucky/Louisville', 'America/Kentucky/Monticello', 'America/Knox_IN', 'America/Kralendijk', 'America/La_Paz', 'America/Lima', 'America/Los_Angeles', 'America/Louisville', 'America/Lower_Princes', 'America/Maceio', 'America/Managua', 'America/Manaus', 'America/Marigot', 'America/Martinique', 'America/Matamoros', 'America/Mazatlan', 'America/Mendoza', 'America/Menominee', 'America/Merida', 'America/Metlakatla', 'America/Mexico_City', 'America/Miquelon', 'America/Moncton', 'America/Monterrey', 'America/Montevideo', 'America/Montreal', 'America/Montserrat', 'America/Nassau', 'America/New_York', 'America/Nipigon', 'America/Nome', 'America/Noronha', 'America/North_Dakota/Beulah', 'America/North_Dakota/Center', 'America/North_Dakota/New_Salem', 'America/Ojinaga', 'America/Panama', 'America/Pangnirtung', 'America/Paramaribo', 'America/Phoenix', 'America/Port-au-Prince', 'America/Port_of_Spain', 'America/Porto_Acre', 'America/Porto_Velho', 'America/Puerto_Rico', 'America/Punta_Arenas', 'America/Rainy_River', 'America/Rankin_Inlet', 'America/Recife', 'America/Regina', 'America/Resolute', 'America/Rio_Branco', 'America/Rosario', 'America/Santa_Isabel', 'America/Santarem', 'America/Santiago', 'America/Santo_Domingo', 'America/Sao_Paulo', 'America/Scoresbysund', 'America/Shiprock', 'America/Sitka', 'America/St_Barthelemy', 'America/St_Johns', 'America/St_Kitts', 'America/St_Lucia', 'America/St_Thomas', 'America/St_Vincent', 'America/Swift_Current', 'America/Tegucigalpa', 'America/Thule', 'America/Thunder_Bay', 'America/Tijuana', 'America/Toronto', 'America/Tortola', 'America/Vancouver', 'America/Virgin', 'America/Whitehorse', 'America/Winnipeg', 'America/Yakutat', 'America/Yellowknife', 'Antarctica/Casey', 'Antarctica/Davis', 'Antarctica/DumontDUrville', 'Antarctica/Macquarie', 'Antarctica/Mawson', 'Antarctica/McMurdo', 'Antarctica/Palmer', 'Antarctica/Rothera', 'Antarctica/South_Pole', 'Antarctica/Syowa', 'Antarctica/Troll', 'Antarctica/Vostok', 'Arctic/Longyearbyen', 'Asia/Aden', 'Asia/Almaty', 'Asia/Amman', 'Asia/Anadyr', 'Asia/Aqtau', 'Asia/Aqtobe', 'Asia/Ashgabat', 'Asia/Ashkhabad', 'Asia/Atyrau', 'Asia/Baghdad', 'Asia/Bahrain', 'Asia/Baku', 'Asia/Bangkok', 'Asia/Barnaul', 'Asia/Beirut', 'Asia/Bishkek', 'Asia/Brunei', 'Asia/Calcutta', 'Asia/Chita', 'Asia/Choibalsan', 'Asia/Chongqing', 'Asia/Chungking', 'Asia/Colombo', 'Asia/Dacca', 'Asia/Damascus', 'Asia/Dhaka', 'Asia/Dili', 'Asia/Dubai', 'Asia/Dushanbe', 'Asia/Famagusta', 'Asia/Gaza', 'Asia/Harbin', 'Asia/Hebron', 'Asia/Ho_Chi_Minh', 'Asia/Hong_Kong', 'Asia/Hovd', 'Asia/Irkutsk', 'Asia/Istanbul', 'Asia/Jakarta', 'Asia/Jayapura', 'Asia/Jerusalem', 'Asia/Kabul', 'Asia/Kamchatka', 'Asia/Karachi', 'Asia/Kashgar', 'Asia/Kathmandu', 'Asia/Katmandu', 'Asia/Khandyga', 'Asia/Kolkata', 'Asia/Krasnoyarsk', 'Asia/Kuala_Lumpur', 'Asia/Kuching', 'Asia/Kuwait', 'Asia/Macao', 'Asia/Macau', 'Asia/Magadan', 'Asia/Makassar', 'Asia/Manila', 'Asia/Muscat', 'Asia/Nicosia', 'Asia/Novokuznetsk', 'Asia/Novosibirsk', 'Asia/Omsk', 'Asia/Oral', 'Asia/Phnom_Penh', 'Asia/Pontianak', 'Asia/Pyongyang', 'Asia/Qatar', 'Asia/Qyzylorda', 'Asia/Rangoon', 'Asia/Riyadh', 'Asia/Saigon', 'Asia/Sakhalin', 'Asia/Samarkand', 'Asia/Seoul', 'Asia/Shanghai', 'Asia/Singapore', 'Asia/Srednekolymsk', 'Asia/Taipei', 'Asia/Tashkent', 'Asia/Tbilisi', 'Asia/Tehran', 'Asia/Tel_Aviv', 'Asia/Thimbu', 'Asia/Thimphu', 'Asia/Tokyo', 'Asia/Tomsk', 'Asia/Ujung_Pandang', 'Asia/Ulaanbaatar', 'Asia/Ulan_Bator', 'Asia/Urumqi', 'Asia/Ust-Nera', 'Asia/Vientiane', 'Asia/Vladivostok', 'Asia/Yakutsk', 'Asia/Yangon', 'Asia/Yekaterinburg', 'Asia/Yerevan', 'Atlantic/Azores', 'Atlantic/Bermuda', 'Atlantic/Canary', 'Atlantic/Cape_Verde', 'Atlantic/Faeroe', 'Atlantic/Faroe', 'Atlantic/Jan_Mayen', 'Atlantic/Madeira', 'Atlantic/Reykjavik', 'Atlantic/South_Georgia', 'Atlantic/St_Helena', 'Atlantic/Stanley', 'Australia/ACT', 'Australia/Adelaide', 'Australia/Brisbane', 'Australia/Broken_Hill', 'Australia/Canberra', 'Australia/Currie', 'Australia/Darwin', 'Australia/Eucla', 'Australia/Hobart', 'Australia/LHI', 'Australia/Lindeman', 'Australia/Lord_Howe', 'Australia/Melbourne', 'Australia/NSW', 'Australia/North', 'Australia/Perth', 'Australia/Queensland', 'Australia/South', 'Australia/Sydney', 'Australia/Tasmania', 'Australia/Victoria', 'Australia/West', 'Australia/Yancowinna', 'Brazil/Acre', 'Brazil/DeNoronha', 'Brazil/East', 'Brazil/West', 'CET', 'CST6CDT', 'Canada/Atlantic', 'Canada/Central', 'Canada/Eastern', 'Canada/Mountain', 'Canada/Newfoundland', 'Canada/Pacific', 'Canada/Saskatchewan', 'Canada/Yukon', 'Chile/Continental', 'Chile/EasterIsland', 'Cuba', 'EET', 'EST', 'EST5EDT', 'Egypt', 'Eire', 'Etc/GMT', 'Etc/GMT+0', 'Etc/GMT+1', 'Etc/GMT+10', 'Etc/GMT+11', 'Etc/GMT+12', 'Etc/GMT+2', 'Etc/GMT+3', 'Etc/GMT+4', 'Etc/GMT+5', 'Etc/GMT+6', 'Etc/GMT+7', 'Etc/GMT+8', 'Etc/GMT+9', 'Etc/GMT-0', 'Etc/GMT-1', 'Etc/GMT-10', 'Etc/GMT-11', 'Etc/GMT-12', 'Etc/GMT-13', 'Etc/GMT-14', 'Etc/GMT-2', 'Etc/GMT-3', 'Etc/GMT-4', 'Etc/GMT-5', 'Etc/GMT-6', 'Etc/GMT-7', 'Etc/GMT-8', 'Etc/GMT-9', 'Etc/GMT0', 'Etc/Greenwich', 'Etc/UCT', 'Etc/UTC', 'Etc/Universal', 'Etc/Zulu', 'Europe/Amsterdam', 'Europe/Andorra', 'Europe/Astrakhan', 'Europe/Athens', 'Europe/Belfast', 'Europe/Belgrade', 'Europe/Berlin', 'Europe/Bratislava', 'Europe/Brussels', 'Europe/Bucharest', 'Europe/Budapest', 'Europe/Busingen', 'Europe/Chisinau', 'Europe/Copenhagen', 'Europe/Dublin', 'Europe/Gibraltar', 'Europe/Guernsey', 'Europe/Helsinki', 'Europe/Isle_of_Man', 'Europe/Istanbul', 'Europe/Jersey', 'Europe/Kaliningrad', 'Europe/Kiev', 'Europe/Kirov', 'Europe/Lisbon', 'Europe/Ljubljana', 'Europe/London', 'Europe/Luxembourg', 'Europe/Madrid', 'Europe/Malta', 'Europe/Mariehamn', 'Europe/Minsk', 'Europe/Monaco', 'Europe/Moscow', 'Europe/Nicosia', 'Europe/Oslo', 'Europe/Paris', 'Europe/Podgorica', 'Europe/Prague', 'Europe/Riga', 'Europe/Rome', 'Europe/Samara', 'Europe/San_Marino', 'Europe/Sarajevo', 'Europe/Saratov', 'Europe/Simferopol', 'Europe/Skopje', 'Europe/Sofia', 'Europe/Stockholm', 'Europe/Tallinn', 'Europe/Tirane', 'Europe/Tiraspol', 'Europe/Ulyanovsk', 'Europe/Uzhgorod', 'Europe/Vaduz', 'Europe/Vatican', 'Europe/Vienna', 'Europe/Vilnius', 'Europe/Volgograd', 'Europe/Warsaw', 'Europe/Zagreb', 'Europe/Zaporozhye', 'Europe/Zurich', 'GB', 'GB-Eire', 'GMT', 'GMT+0', 'GMT-0', 'GMT0', 'Greenwich', 'HST', 'Hongkong', 'Iceland', 'Indian/Antananarivo', 'Indian/Chagos', 'Indian/Christmas', 'Indian/Cocos', 'Indian/Comoro', 'Indian/Kerguelen', 'Indian/Mahe', 'Indian/Maldives', 'Indian/Mauritius', 'Indian/Mayotte', 'Indian/Reunion', 'Iran', 'Israel', 'Jamaica', 'Japan', 'Kwajalein', 'Libya', 'MET', 'MST', 'MST7MDT', 'Mexico/BajaNorte', 'Mexico/BajaSur', 'Mexico/General', 'NZ', 'NZ-CHAT', 'Navajo', 'PRC', 'PST8PDT', 'Pacific/Apia', 'Pacific/Auckland', 'Pacific/Bougainville', 'Pacific/Chatham', 'Pacific/Chuuk', 'Pacific/Easter', 'Pacific/Efate', 'Pacific/Enderbury', 'Pacific/Fakaofo', 'Pacific/Fiji', 'Pacific/Funafuti', 'Pacific/Galapagos', 'Pacific/Gambier', 'Pacific/Guadalcanal', 'Pacific/Guam', 'Pacific/Honolulu', 'Pacific/Johnston', 'Pacific/Kiritimati', 'Pacific/Kosrae', 'Pacific/Kwajalein', 'Pacific/Majuro', 'Pacific/Marquesas', 'Pacific/Midway', 'Pacific/Nauru', 'Pacific/Niue', 'Pacific/Norfolk', 'Pacific/Noumea', 'Pacific/Pago_Pago', 'Pacific/Palau', 'Pacific/Pitcairn', 'Pacific/Pohnpei', 'Pacific/Ponape', 'Pacific/Port_Moresby', 'Pacific/Rarotonga', 'Pacific/Saipan', 'Pacific/Samoa', 'Pacific/Tahiti', 'Pacific/Tarawa', 'Pacific/Tongatapu', 'Pacific/Truk', 'Pacific/Wake', 'Pacific/Wallis', 'Pacific/Yap', 'Poland', 'Portugal', 'ROC', 'ROK', 'Singapore', 'Turkey', 'UCT', 'US/Alaska', 'US/Aleutian', 'US/Arizona', 'US/Central', 'US/East-Indiana', 'US/Eastern', 'US/Hawaii', 'US/Indiana-Starke', 'US/Michigan', 'US/Mountain', 'US/Pacific', 'US/Pacific-New', 'US/Samoa', 'UTC', 'Universal', 'W-SU', 'WET', 'Zulu'
+];
+
+// Language data (from locales/)
+const LANGUAGES = [
+  { code: 'pcm', name: 'Pidgin' },
+  { code: 'am', name: 'Amharic' },
+  { code: 'ha', name: 'Hausa' },
+  { code: 'ig', name: 'Igbo' },
+  { code: 'sw', name: 'Swahili' },
+  { code: 'yo', name: 'Yoruba' },
+  { code: 'zu', name: 'Zulu' }
+];
+
+// Badge translations per locale (sourced from each locale file)
+const BADGE_TRANSLATIONS = {
+  pcm: { yes: '✅ Gbosa! Do am',     no: '🛑 No be today' },
+  am:  { yes: '✅ ያድርገው!',           no: '🛑 ዛሬ አይደለም' },
+  ha:  { yes: '✅ Yi shi!',           no: '🛑 Ba jiya ba' },
+  ig:  { yes: '✅ Eme ya!',           no: '🛑 Taa ka o gaadi' },
+  sw:  { yes: '✅ Fanya!',            no: '🛑 Sio leo' },
+  yo:  { yes: '✅ Ṣe é!',             no: '🛑 Ìwọ òun lónìí' },
+  zu:  { yes: '✅ Yenza!',            no: '🛑 Hhayi namuhla' }
+};
+
+// UI string translations per locale (timezone_label/lang_label from locale files; buttons are extension-specific)
+const UI_TRANSLATIONS = {
+  pcm: { timezone: 'Awa',         language: 'Langwej',    refresh: '↻ Check Am Again',     website: 'Visit Site' },
+  am:  { timezone: 'ሰዓት',         language: 'ቋንቋ',        refresh: '↻ ደግመው ያረጋግጡ',        website: 'ድረ-ገፁን ጎብኝ' },
+  ha:  { timezone: 'Lokaci',      language: 'Harshe',     refresh: '↻ Duba Kuma',           website: 'Ziyarci Shafin' },
+  ig:  { timezone: 'Oge',         language: 'Asụsụ',      refresh: '↻ Lelee Ọzọ',           website: 'Gaa Saịtị' },
+  sw:  { timezone: 'Saa',         language: 'Lugha',      refresh: '↻ Angalia Tena',         website: 'Tembelea Tovuti' },
+  yo:  { timezone: 'Akoko',       language: 'Ede',        refresh: '↻ Ṣàyẹ̀wò Lẹ́ẹ̀kansi',  website: 'Ṣàbẹ̀wò Ààyé' },
+  zu:  { timezone: 'Isikhathi',   language: 'Ulimi',      refresh: '↻ Hlola Futhi',          website: 'Vakashela Isayithi' }
+};
+
+function updateUIStrings(lng) {
+  const t = UI_TRANSLATIONS[lng] || UI_TRANSLATIONS['pcm'];
+  document.querySelector('label[for="timezone"]').textContent = t.timezone;
+  document.querySelector('label[for="language"]').textContent = t.language;
+  document.getElementById('refreshBtn').textContent = t.refresh;
+  document.getElementById('websiteBtn').textContent = t.website;
+}
+
+// Initialize timezone select
+function initializeTimezoneSelect() {
+    const tzSelect = document.getElementById('timezone');
+    console.log('Initializing timezone select, element:', tzSelect);
+    
+    TIMEZONES.forEach(tz => {
+        const option = document.createElement('option');
+        option.value = tz;
+        option.textContent = tz;
+        tzSelect.appendChild(option);
+    });
+    
+    console.log('Added all timezones, total:', TIMEZONES.length);
+    
+    // Load saved timezone from localStorage
+    const savedTz = localStorage.getItem('timezone');
+    if (savedTz && TIMEZONES.includes(savedTz)) {
+        tzSelect.value = savedTz;
+        console.log('Loaded saved timezone:', savedTz);
+    } else {
+        tzSelect.value = 'UTC';
+        console.log('No saved timezone, using UTC');
+    }
+}
+
+// Initialize language select
+function initializeLanguageSelect() {
+    const langSelect = document.getElementById('language');
+    console.log('Initializing language select, element:', langSelect);
+    
+    LANGUAGES.forEach((lang) => {
+        const option = document.createElement('option');
+        option.value = lang.code;
+        option.textContent = `${lang.name} (${lang.code})`;
+        langSelect.appendChild(option);
+    });
+    
+    console.log('Added all languages, total:', LANGUAGES.length);
+    
+    // Load saved language from localStorage
+    const savedLang = localStorage.getItem('language');
+    if (savedLang && LANGUAGES.some(l => l.code === savedLang)) {
+        langSelect.value = savedLang;
+        console.log('Loaded saved language:', savedLang);
+    } else {
+        langSelect.value = 'pcm';
+        console.log('No saved language, using Pidgin');
+    }
+}
+
 // Fetch deployment decision from API
 async function fetchDeploymentDecision() {
     const contentDiv = document.getElementById('content');
-    const timezoneInput = document.getElementById('timezone');
-    const timezone = timezoneInput.value || 'UTC';
+    const timezoneSelect = document.getElementById('timezone');
+    const languageSelect = document.getElementById('language');
+    const timezone = timezoneSelect.value || 'UTC';
+    const language = languageSelect.value || 'en';
 
-    contentDiv.innerHTML = '<div class="loading">⏳ Checking the vibes...</div>';
+    console.log('Fetching decision with:', { timezone, language });
+    contentDiv.innerHTML = '<div class="loading">Checking the vibes...</div>';
 
     try {
-        const apiUrl = `https://makeideploy.today/api?tz=${encodeURIComponent(timezone)}`;
+        const apiUrl = `https://makeideploy.today/api?tz=${encodeURIComponent(timezone)}&lng=${encodeURIComponent(language)}`;
+        console.log('API URL:', apiUrl);
+        
         const response = await fetch(apiUrl);
 
         if (!response.ok) {
@@ -15,8 +116,10 @@ async function fetchDeploymentDecision() {
         }
 
         const data = await response.json();
+        console.log('API Response:', data);
         displayResult(data);
     } catch (error) {
+        console.error('Error:', error);
         contentDiv.innerHTML = `
             <div class="error">
                 ❌ Oops! Error checking vibes: ${error.message}
@@ -28,14 +131,26 @@ async function fetchDeploymentDecision() {
 // Display the decision result
 function displayResult(data) {
     const contentDiv = document.getElementById('content');
-    const decision = data.makeideploy ? '✨ YES! DEPLOY! ✨' : '🛑 HOLD UP! NOT TODAY 🛑';
-    const decisionClass = data.makeideploy ? 'green' : 'red';
+    const canDeploy = data.makeideploy;
+    const lng = data.lng;
+
+    document.getElementById('tagline').textContent = data.tagline;
+    updateUIStrings(lng);
+
+    if (canDeploy) {
+        document.body.classList.remove('state-no-deploy');
+    } else {
+        document.body.classList.add('state-no-deploy');
+    }
+
+    const t = BADGE_TRANSLATIONS[lng] || BADGE_TRANSLATIONS['pcm'];
+    const badge = canDeploy ? t.yes : t.no;
 
     contentDiv.innerHTML = `
-        <div class="decision ${decisionClass}">${decision}</div>
-        <div class="message">${data.message}</div>
-        <div class="metadata">
-            📍 ${data.timezone} | 🌍 ${data.lng.toUpperCase()}
+        <div>
+            <div class="reason">${data.message}</div>
+            <div class="badge">${badge}</div>
+            <div class="metadata">📍 ${data.timezone} · ${lng.toUpperCase()}</div>
         </div>
     `;
 }
@@ -46,7 +161,29 @@ document.getElementById('websiteBtn').addEventListener('click', () => {
     chrome.tabs.create({ url: 'https://makeideploy.today' });
 });
 
-document.getElementById('timezone').addEventListener('change', fetchDeploymentDecision);
+document.getElementById('timezone').addEventListener('change', (e) => {
+    localStorage.setItem('timezone', e.target.value);
+    fetchDeploymentDecision();
+});
 
-// Load decision on popup open
-fetchDeploymentDecision();
+document.getElementById('language').addEventListener('change', (e) => {
+    localStorage.setItem('language', e.target.value);
+    updateUIStrings(e.target.value);
+    fetchDeploymentDecision();
+});
+
+// Initialize on popup open
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', () => {
+        console.log('DOMContentLoaded: Initializing selects');
+        initializeTimezoneSelect();
+        initializeLanguageSelect();
+        setTimeout(fetchDeploymentDecision, 100);
+    });
+} else {
+    // DOM is already loaded
+    console.log('DOM already loaded: Initializing selects');
+    initializeTimezoneSelect();
+    initializeLanguageSelect();
+    setTimeout(fetchDeploymentDecision, 100);
+}
